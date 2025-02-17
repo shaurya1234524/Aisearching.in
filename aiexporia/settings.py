143da535d5c168118c12b72c8fa65231665f3fdb
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-02-17 23:09:52 +0530

    Update settings.py

--- aiexporia/settings.py
+++ aiexporia/settings.py
@@ -152,6 +152,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,"aiexporia/static"),
+    os.path.join(BASE_DIR,"static"),
 )
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
