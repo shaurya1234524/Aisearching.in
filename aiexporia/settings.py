0a3820f9a9e4925b2bbbfc2dfb06b5591ee2770e
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-02-18 17:40:41 +0530

    Update settings.py

--- aiexporia/settings.py
+++ aiexporia/settings.py
@@ -49,7 +49,7 @@ AWS_SECRET_ACCESS_KEY = 'epCjX2suMhiHE9yc8jXpvpK7WdIDXv08gsHbfSaU'
 
 MIDDLEWARE = [
         'django.middleware.common.CommonMiddleware',  # This is important to leave in place
-    'aiexporia.middleware.TrailingSlashMiddleware',   # Add this line here
+    'aiexporiahome.middleware.TrailingSlashMiddleware',   # Add this line here
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
