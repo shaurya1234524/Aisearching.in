75f2cb0869af3712aeeafe943d1989670cdbb313
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-02-21 00:32:49 +0530

    Update urls.py

--- aiexporia/urls.py
+++ aiexporia/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('HomeworkAi',views.HomeworkAi,name="HomeworkAi"),
   path('sitemap.xml',views.sitemap,name="sitemap"),
  path("robots.txt", views.robots_txt, name="robots_txt"),
-  path("ai-analysis/<str:tool_name>",views.analyzing,name="analyzing"),
+
  path("ai-analysis/<str:tool_name>",views.ai_tool_analysis,name="ai_tool_analysis"),
     path('searchresult',views.searchresults,name="searchresults"),
     path('searchres',views.searchres,name="searchres"),
