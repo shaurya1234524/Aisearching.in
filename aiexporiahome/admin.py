e43a633e19873de31a0bef7be6b323b6fc8178ab
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-07-16 15:13:32 +0530

    Delete aiexporiahome directory

--- aiexporiahome/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from aiexporiahome.models import AI ,blog,indianAI,AIToolRating,AIToolAdvantage,AIToolAdvantage
-admin.site.register(AI)
-admin.site.register(blog)
-
